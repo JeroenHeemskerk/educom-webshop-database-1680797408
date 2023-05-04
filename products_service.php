9d6ca23f67ec472f6facc6d151ab1720c5ea21e1
Hana Alkadri <hanaalkadry@gmail.com>
2023-05-04 14:40:58 +0200

    #14

--- products_service.php
+++ products_service.php
@@ -1,11 +1,17 @@
 <?php
 include_once 'database_repository.php';
-function getWebshopData(){
-    $allProducts = getAllProducts();
-
-    return ["allProducts" => $allProducts];
-}
-
+function getWebshopData(){ 
+    $allProducts = array();
+    $genericErr = "";
+    try {
+        $allProducts = getAllProducts(); 
+    } 
+    catch (Exception $e) {
+        $genericErr = "Sorry, er is een technische storing bij het ophalen van de producten";
+        logToServer("get products failed" .  $e->getMessage());
+    }  
+    return ["allProducts" => $allProducts, "genericErr" => $genericErr]; 
+} 
 
 function getDetailData($productId){
     $product = getProductById($productId);
